038ab96163afe8596945b4027549c61e709ceb0e
Joaquin Pozo <joaquin.pozo@usach.cl>
2024-05-15 18:24:17 -0400

    Implementación del Req. 4: TDA Line

--- main.pl
+++ main.pl
@@ -12,7 +12,7 @@ station(Id, Name, Type, StopTime, [Id, Name, Type, StopTime]) :-
 
 % Req. 2: TDA section - constructor
 % Meta Primaria: section/5
-% Meta Secundaria: (float(Distance); integer(Distance)),Distance > 0,(float(Cost); integer(Cost)),Cost >= 0,!.
+% Meta Secundaria: (float(Distance); integer(Distance)),Distance > 0,(float(Cost); integer(Cost)),Cost >= 0,!
 section(Point1, Point2, Distance, Cost, [Point1, Point2, Distance, Cost]) :-
     (float(Distance); integer(Distance)),
     Distance > 0,
@@ -22,13 +22,29 @@ section(Point1, Point2, Distance, Cost, [Point1, Point2, Distance, Cost]) :-
 
 % Req. 3: TDA line - constructor
 % Meta Primaria: line/5
-% Meta Secundaria: integer(Id),string(Name),string(RailType),is_list(Sections).
+% Meta Secundaria: integer(Id),string(Name),string(RailType),is_list(Sections)
 line(Id, Name, RailType, Sections, [Id, Name, RailType, Sections]) :-
     integer(Id),
     string(Name),
     string(RailType),
     is_list(Sections).
 % Req. 4: TDA line - Otros predicados
-% Meta Primaria:
-% Meta Secundaria:
-lineLength(Line, Length, Distance, Cost).
+% Meta Primaria: lineLength/4
+% Meta Secundaria: line/5, lineLengthRec/4
+lineLength(Line, Length, Distance, Cost) :-
+    line(_, _, _, Sections, Line),
+    lineLengthRec(Sections, Length, Distance, Cost).
+
+% TDA line- Otros predicados: caso base para función recursiva
+% Meta Primaria: lineLength/4
+% Meta Secundaria: N/A
+lineLengthRec([], 0, 0, 0).
+% TDA line- Otros predicados: caso recursivo para función recursiva
+% Meta Primaria: lineLength/4
+% Meta Secundaria: section/5, lingeLengthRec/4, Distance is GetDistanceAux + GetDistance,Cost is GetCostAux + GetCost,Length is GetLengthAux + 1
+lineLengthRec([Section | Sections], Length, Distance, Cost) :-
+    section(_, _, GetDistance, GetCost, Section),
+    lineLengthRec(Sections, GetLengthAux, GetDistanceAux, GetCostAux),
+    Distance is GetDistanceAux + GetDistance,
+    Cost is GetCostAux + GetCost,
+    Length is GetLengthAux + 1.
