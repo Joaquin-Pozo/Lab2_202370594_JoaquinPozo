3523482b4e06c0621c25e4faf8e545e73c95a6b0
Joaquin Pozo <joaquin.pozo@usach.cl>
2024-05-18 19:58:10 -0400

    Implementacion de Requerimientos 5 y 6

--- main.pl
+++ main.pl
@@ -1,4 +1,4 @@
-% Req. 1: TDA station - constructor
+% Req. 2: TDA station - constructor
 % Meta Primaria: station/5
 % Meta Secundaria: integer(Id),string(Name),string(Type),(Type == "r"; Type == "m"; Type == "c"; Type == "t"),integer(StopTime),StopTime >= 0,!.
 station(Id, Name, Type, StopTime, [Id, Name, Type, StopTime]) :-
@@ -10,7 +10,7 @@ station(Id, Name, Type, StopTime, [Id, Name, Type, StopTime]) :-
     StopTime >= 0,
     !.
 
-% Req. 2: TDA section - constructor
+% Req. 3: TDA section - constructor
 % Meta Primaria: section/5
 % Meta Secundaria: (float(Distance); integer(Distance)),Distance > 0,(float(Cost); integer(Cost)),Cost >= 0,!
 section(Point1, Point2, Distance, Cost, [Point1, Point2, Distance, Cost]) :-
@@ -20,7 +20,7 @@ section(Point1, Point2, Distance, Cost, [Point1, Point2, Distance, Cost]) :-
     Cost >= 0,
     !.
 
-% Req. 3: TDA line - constructor
+% Req. 4: TDA line - constructor
 % Meta Primaria: line/5
 % Meta Secundaria: integer(Id),string(Name),string(RailType),is_list(Sections)
 line(Id, Name, RailType, Sections, [Id, Name, RailType, Sections]) :-
@@ -28,13 +28,12 @@ line(Id, Name, RailType, Sections, [Id, Name, RailType, Sections]) :-
     string(Name),
     string(RailType),
     is_list(Sections).
-% Req. 4: TDA line - Otros predicados
+% Req. 5: TDA line - Otros predicados
 % Meta Primaria: lineLength/4
 % Meta Secundaria: line/5, lineLengthRec/4
 lineLength(Line, Length, Distance, Cost) :-
     line(_, _, _, Sections, Line),
     lineLengthRec(Sections, Length, Distance, Cost).
-
 % TDA line- Otros predicados: caso base para función recursiva
 % Meta Primaria: lineLength/4
 % Meta Secundaria: N/A
@@ -48,3 +47,32 @@ lineLengthRec([Section | Sections], Length, Distance, Cost) :-
     Distance is GetDistanceAux + GetDistance,
     Cost is GetCostAux + GetCost,
     Length is GetLengthAux + 1.
+
+% Req. 6: TDA line - otras funciones
+% Meta Primaria: lineSectionLength/5
+% Meta Secundaria: line/5, lineSectionLengthRec/6
+lineSectionLength(Line, Station1Name, Station2Name, Distance, Cost) :-
+    line(_, _, _, Sections, Line),
+    lineSectionLengthRec(Sections, Station1Name, Station2Name, Distance, Cost, 0).
+
+lineSectionLengthRec([], _, _, 0, 0, _).
+lineSectionLengthRec([Section | Sections], Station1Name, Station2Name, Distance, Cost, Flag) :-
+    section(GetStation1, GetStation2, GetDistance, GetCost, Section),
+    station(_, GetStation1Name, _, _, GetStation1),
+    station(_, GetStation2Name, _, _, GetStation2),
+    (   (GetStation1Name = Station1Name, GetStation2Name = Station2Name) ->
+            (Distance is GetDistance, Cost is GetCost)
+    ;   (GetStation1Name = Station1Name) ->
+            lineSectionLengthRec(Sections, GetStation2Name, Station2Name, DistanceAux, CostAux, 1),
+            Distance is DistanceAux + GetDistance,
+            Cost is CostAux + GetCost
+    ;   (GetStation2Name = Station2Name) ->
+            lineSectionLengthRec(Sections, GetStation1Name, Station2Name, DistanceAux, CostAux, 0),
+            Distance is DistanceAux + GetDistance,
+            Cost is CostAux + GetCost
+    ;   (Flag = 1 ->
+            lineSectionLengthRec(Sections, Station1Name, Station2Name, DistanceAux, CostAux, Flag),
+            Distance is DistanceAux + GetDistance,
+            Cost is CostAux + GetCost)
+    ;   lineSectionLengthRec(Sections, Station1Name, Station2Name, Distance, Cost, Flag)
+    ).
--- script_pruebas.pl
+++ script_pruebas.pl
@@ -27,4 +27,5 @@ section(ST10, ST11, 7, 200, S10),
 line(0, "Línea 0", "UIC 60 ASCE", [ ], L0),
 line(1, "Línea 1", "100 R.E.", [S0, S1, S2, S3, S4, S5, S6, S7, S8, S9, S10], L1),
 lineLength(L0, LargoLinea0, DistanciaLinea0, CostoLinea0),
-lineLength(L1, LargoLinea1, DistanciaLinea1, CostoLinea1).
+lineLength(L1, LargoLinea1, DistanciaLinea1, CostoLinea1),
+lineSectionLength(L1, "Estación Central", "La Moneda", DISTANCIA, COSTO).
